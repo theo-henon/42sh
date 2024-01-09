6371ce9372a6844f2480e7eb2043c40292b768b2
Théo Hénon <=>
2024-01-09 20:32:07 +0100

    tests(functional): change 42sh executable path in config

--- tests/functional/config.py
+++ tests/functional/config.py
@@ -1,5 +1,5 @@
 REFSH_STRINPUT = ["bash", "--posix", "-c"]
 REFSH_FILEINPUT = ["bash", "--posix"]
-SH_STRINPUT = ["./42sh", "-c"]
-SH_FILEINPUT = ["./42sh"]
-SCRIPTS_DIR = "./inputs"
+SH_STRINPUT = ["./src/42sh", "-c"]
+SH_FILEINPUT = ["./src/42sh"]
+SCRIPTS_DIR = "./tests/functional/inputs"
