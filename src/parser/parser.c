c33b48e3a15ac57b0bf2f1b58899cf76b990840f
Etienne Reverchon <etienne.reverchon@epita.fr>
2024-01-11 21:11:18 +0100

    feat(parser): start

---
+++ src/parser/parser.c
@@ -0,0 +1,54 @@
+#include "parser.h"
+
+
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+struct ast *parse_input(struct lexer *lexer)
+{
+    if (lexer->current->type == SIMPLE_CMD)
+    {
+        int i = 0;
+        char **args = calloc(1, strlen(lexer->current->value));
+        strcpy(args[i], lexer->current->value);
+        lexer_pop(lexer);
+        while (lexer->current->type == TOKEN_WORD)
+        {
+            args = realloc(args, sizeof(args) + strlen(lexer->current->value));
+            i++;
+            strcpy(args[i], lexer->current->value);
+            lexer_pop(lexer);
+        }
+    }
+    else if (lexer->current->type == IF_CLAUSE)
+    {
+        
+    }
+    /*
+    switch (lexer->current->type)
+    {
+        case SIMPLE_CMD:
+            char **args = calloc(1, strlen(lexer->current->value));
+            strcpy(args[0], lexer->current->value);
+
+            struct token *tmp = lexer_pop(lexer);
+            if (tmp)
+            create_simple_cmd(char **args)
+            break;
+        case TOKEN_IF:
+            create_if_clause(struct base *condition, struct base *then_body, struct base *else_body)
+            break;
+        case 
+
+    }
+    */
+
+}
+
+struct simple_cmd *parse_simple_cmd(struct lexer lexer);
+struct list *parse_list(struct lexer lexer);
+struct if_clause *parse_if_clause(struct lexer lexer);
