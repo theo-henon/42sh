3ad5329384d363fbd411a76e1f7cc233f4899abf
Théo <theo.henon@epita.fr>
2024-01-13 17:03:02 +0100

    tests(functional): fix exec_formstring and exec_from_file methods

--- tests/functional/shell_script.py
+++ tests/functional/shell_script.py
@@ -14,29 +14,40 @@ class ShellScript():
         content = file.read()
         return content
 
+    def __outdiff(self, ref_out, out):
+        return f"Expected stdout content : {str(ref_out)}, but got: {str(out)}"
+    
+    def __codediff(self, refcode, code):
+        f"Expected exit code: {refcode}, but got exit code: {code}"
+
     def exec_from_string(self):
         ref_args = config.REFSH_STRINPUT.copy()
         ref_args.append(self.content)
-        ref_out = subprocess.run(ref_args, subprocess.PIPE)
+        ref_out = subprocess.run(ref_args, stdout=subprocess.PIPE)
 
         args = config.SH_STRINPUT.copy()
         args.append(self.content)
-        out = subprocess.run(args, subprocess.PIPE)
+        out = subprocess.run(args, stdout=subprocess.PIPE)
 
-        assert ref_out.stdout == out.stdout
-        assert ref_out.returncode == out.returncode
+        if ref_out.stdout != out.stdout:
+            raise AssertionError(self.__outdiff(ref_out.stdout, out.stdout))
+        if ref_out.returncode != out.returncode:
+            raise AssertionError(self.__codediff(ref_out.returncode, out.returncode))
+
 
     def exec_from_file(self):
         ref_args = config.REFSH_FILEINPUT.copy()
         ref_args.append(self.filename)
-        ref_out = subprocess.run(ref_args, subprocess.PIPE)
+        ref_out = subprocess.run(ref_args, stdout=subprocess.PIPE)
 
         args = config.SH_FILEINPUT.copy()
         args.append(self.filename)
-        out = subprocess.run(args, subprocess.PIPE)
+        out = subprocess.run(args, stdout=subprocess.PIPE)
 
-        assert ref_out.stdout == out.stdout 
-        assert ref_out.returncode == out.returncode
+        if ref_out.stdout != out.stdout:
+            raise AssertionError(self.__outdiff(ref_out.stdout, out.stdout))
+        if ref_out.returncode != out.returncode:
+            raise AssertionError(self.__codediff(ref_out.returncode, out.returncode))
 
 
     def exec_from_stdin(self):
@@ -47,5 +58,7 @@ class ShellScript():
         p = subprocess.Popen(config.SH_FILEINPUT, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout = p.communicate(input=encoded_content)[0]
 
-        assert ref_stdout == stdout 
-        assert ref_p.returncode == p.returncode
+        if ref_stdout != stdout:
+            raise AssertionError(self.__outdiff(ref_stdout, stdout))
+        if ref_p.returncode != p.returncode:
+            raise AssertionError(self.__codediff(ref_p.returncode, p.returncode))
