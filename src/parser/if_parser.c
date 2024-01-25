ecabe0707186a038224994c727b4b0d51caf48fb
Steven Delalain <steven.delalain@epita.fr>
2024-01-25 22:06:36 +0100

    feat(parser): if with only else and not elif

--- src/parser/if_parser.c
+++ src/parser/if_parser.c
@@ -1,3 +1,39 @@
 #include "if_parser.h"
 
-#include <stdlib.h>
+#include "list_parser.h"
+
+struct if_clause *parse_if_clause(struct parser *parser)
+{
+    struct list *condition = parse_compound_list(parser);
+    struct token *token = lexer_peek(parser->lexer);
+
+    if (token->type != TOKEN_THEN || !condition)
+    {
+        parser->status = PARSER_UNEXPECTED_TOKEN;
+        list_free(condition);
+        return NULL;
+    }
+    token = lexer_pop(parser->lexer);
+
+    struct list *body = parse_compound_list(parser);
+    token = lexer_peek(parser->lexer);
+
+    if (token->type == TOKEN_ELSE && body != NULL)
+    {
+        struct else_clause *else_clause =
+            else_clause_create(NULL, parse_compound_list(parser));
+        return if_clause_create(condition, body, else_clause);
+    }
+    else if (token->type == TOKEN_ELIF)
+    {
+        // TODO : case elif
+    }
+    else
+    {
+        parser->status = PARSER_UNEXPECTED_TOKEN;
+        list_free(condition);
+        list_free(body);
+        return NULL;
+    }
+    return NULL;
+}
---
+++ src/visitor/if_visitor.h
@@ -0,0 +1,4 @@
+#ifndef IF_VISITOR_H
+#define IF_VISITOR_H
+
+#endif /* IF_VISITOR_H A */
