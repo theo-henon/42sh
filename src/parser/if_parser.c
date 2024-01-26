571573be79a3cdf8c767f5604261e8ded07d75b3
Steven Delalain <steven.delalain@epita.fr>
2024-01-26 17:52:58 +0100

    feat(parser): wip if_parser

--- src/parser/if_parser.c
+++ src/parser/if_parser.c
@@ -2,6 +2,61 @@
 
 #include "list_parser.h"
 
+struct else_clause *parse_elif_clause(struct parser *parser)
+{
+    struct token *token = lexer_peek(parser->lexer);
+    token = lexer_pop(parser->lexer);
+    struct list *condition = parse_compound_list(parser);
+    token = lexer_peek(parser->lexer);
+    if (condition == NULL || token->type != TOKEN_THEN)
+    {
+        parser->status = PARSER_UNEXPECTED_TOKEN;
+        list_free(condition);
+        return NULL;
+    }
+    struct list *body = parse_compound_list(parser);
+    struct else_clause *else_clause = else_clause_create(condition, body);
+    token = lexer_peek(parser->lexer);
+    struct else_clause *tmp = else_clause->next;
+    while (token->type == TOKEN_ELIF)
+    {
+        token = lexer_pop(parser->lexer);
+        condition = parse_compound_list(parser);
+        token = lexer_peek(parser->lexer);
+        if (condition == NULL || token->type != TOKEN_THEN)
+        {
+            parser->status = PARSER_UNEXPECTED_TOKEN;
+            else_clause_free(else_clause);
+            return NULL;
+        }
+        token = lexer_pop(parser->lexer);
+        body = parse_compound_list(parser);
+        token = lexer_peek(parser->lexer);
+        if (body == NULL)
+        {
+            parser->status = PARSER_UNEXPECTED_TOKEN;
+            else_clause_free(else_clause);
+            list_free(condition);
+            return NULL;
+        }
+        tmp->next = else_clause_create(condition, body);
+        tmp = tmp->next;
+    }
+    if (token->type == TOKEN_ELSE)
+    {
+        tmp->next = else_clause_create(NULL, parse_compound_list(parser));
+    }
+    token = lexer_peek(parser->lexer);
+    if (token->type == TOKEN_FI)
+    {
+        token = lexer_pop(parser->lexer);
+        return else_clause;
+    }
+    parser->status = PARSER_UNEXPECTED_TOKEN;
+    else_clause_free(else_clause);
+    return NULL;
+}
+
 struct if_clause *parse_if_clause(struct parser *parser)
 {
     struct list *condition = parse_compound_list(parser);
@@ -22,11 +77,17 @@ struct if_clause *parse_if_clause(struct parser *parser)
     {
         struct else_clause *else_clause =
             else_clause_create(NULL, parse_compound_list(parser));
+        if (token->type != TOKEN_FI)
+        {
+            parser->status = PARSER_UNEXPECTED_TOKEN;
+            else_clause_free(else_clause);
+            return NULL;
+        }
         return if_clause_create(condition, body, else_clause);
     }
     else if (token->type == TOKEN_ELIF)
     {
-        // TODO : case elif
+        return if_clause_create(condition, body, parse_elif_clause(parser));
     }
     else
     {
