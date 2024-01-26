bbfaa67d951570bbbdab33ea3707af228bd8d7c2
Théo Hénon <theo.henon@epita.fr>
2024-01-26 22:39:06 +0100

    feat(expansion&visitor): expand single quotes in simple_cmd_visit

---
+++ src/expansion/simple_cmd_expansion.h
@@ -0,0 +1,11 @@
+#ifndef SIMPLE_CMD_EXPANSION
+#define SIMPLE_CMD_EXPANSION
+
+#include <stdlib.h>
+
+#include "ast/simple_cmd.h"
+#include "quotes.h"
+
+void expand_simple_cmd(struct simple_cmd *cmd);
+
+#endif // SIMPLE_CMD_EXPANSION
