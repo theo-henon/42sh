4d7e64995e319ff93ee8cb49d9fd57c46fe4d8bd
Théo Hénon <theo.henon@epita.fr>
2024-01-27 13:56:36 +0100

    fix: free all memory also when parse throw an unexpected token error

--- src/42sh.c
+++ src/42sh.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
     }
 
     if (parser->status == PARSER_UNEXPECTED_TOKEN)
-        return 2;
+        code = 2;
 
     input_free(input);
     lexer_free(lexer);
