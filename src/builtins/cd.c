e2dc515cbdd504c79bb0af0cef8dbefd0251ab4a
Etienne Reverchon <etienne.reverchon@epita.fr>
2024-01-29 19:41:51 +0100

    fix(builtins): fix bugs in builtin cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+
 
 #include "builtins.h"
 
 int builtin_cd(char **argv, __attribute__((unused)) struct visitor *visitor)
 {
+
+    char *home = getenv("HOME");
+    if (strcmp(argv[1], "-") == 0)
+    {
+        printf("%s", home);
+        return 0;
+    }
     if (argv[1] == NULL)
     {
-        char *home = getenv("HOME");
         if (home != NULL)
         {
             if (chdir(home) != 0)
             {
                 fprintf(stderr, "Error chdir ! \n");
-                return -1;
+                return 1;
             }
         }
         else
         {
             printf("cd: HOME not set\n");
-            return -1;
+            return 1;
         }
     }
     else
@@ -28,7 +36,7 @@ int builtin_cd(char **argv, __attribute__((unused)) struct visitor *visitor)
         if (chdir(argv[1]) != 0)
         {
             fprintf(stderr, "Error chdir !\n");
-            return -1;
+            return 1;
         }
     }
     return 0;
