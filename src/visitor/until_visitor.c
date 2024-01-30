5ce578ccce7f709a2ccce6ec70218fdcae322ce2
Théo Hénon <theo.henon@epita.fr>
2024-01-30 21:48:33 +0100

    fix(steven): rtn_code

--- src/visitor/until_visitor.c
+++ src/visitor/until_visitor.c
@@ -2,7 +2,7 @@
 
 int until_visit(struct visitor *visitor, struct until_clause *until_clause)
 {
-    int rtn_code;
+    int rtn_code = 0;
     while (visitor->list_visit(visitor, until_clause->condition) != 0)
         rtn_code = visitor->list_visit(visitor, until_clause->body);
     return rtn_code;
--- src/visitor/while_visitor.c
+++ src/visitor/while_visitor.c
@@ -2,7 +2,7 @@
 
 int while_visit(struct visitor *visitor, struct while_clause *while_clause)
 {
-    int rtn_code;
+    int rtn_code = 0;
     while (visitor->list_visit(visitor, while_clause->condition) == 0)
         rtn_code = visitor->list_visit(visitor, while_clause->body);
     return rtn_code;
