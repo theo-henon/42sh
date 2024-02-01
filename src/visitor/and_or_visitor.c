058dce2133e3709ba6044f75eeec1696b8829106
Théo Hénon <theo.henon@epita.fr>
2024-02-01 21:41:17 +0100

    fix(visitor): and_or visitor

--- src/visitor/and_or_visitor.c
+++ src/visitor/and_or_visitor.c
@@ -4,17 +4,31 @@
 
 int and_or_visit(struct visitor *visitor, struct and_or *and_or)
 {
-    if (and_or->pipeline != NULL)
+    if (and_or == NULL)
+        return 0;
+    else if (and_or->pipeline != NULL)
         return visitor->pipeline_visit(visitor, and_or->pipeline);
     else if (and_or->op == TOKEN_AND)
-        return !(and_or_visit(visitor, and_or->left)
-                 && and_or_visit(visitor, and_or->right));
+    {
+        int first = and_or_visit(visitor, and_or->left);
+        if (first != 0)
+            return first;
+
+        int second = and_or_visit(visitor, and_or->right);
+        if (second != 0)
+            return second;
+        return 0;
+    }
     else
     {
-        int code = and_or_visit(visitor, and_or->left);
-        if (code == 0)
-            return code;
-        else
-            return !(code || and_or_visit(visitor, and_or->right));
+        int first = and_or_visit(visitor, and_or->left);
+        if (first == 0)
+            return 0;
+
+        int second = and_or_visit(visitor, and_or->right);
+        if (second != 0)
+            return second;
+
+        return first != 0 && second != 0;
     }
 }
